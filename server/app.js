64e3b16964dcb68a451082b4cfb64fb7a1bdaeb1
mdramirez071 <mdramirez071@gmail.com>
2019-09-01 12:40:13 -0700

    removes unnecessary syntax and adjusts minor formatting for project files

--- server/app.js
+++ server/app.js
@@ -1,59 +1,39 @@
 const express = require('express');
-//const http = require('http');
 const fs = require('fs');
 const app = express();
-// const { Console } = require('console');
-// const output = fs.createWriteStream('./stdout.log');
-// const errorOutput = fs.createWriteStream('./stderr.log');
-// // Custom simple logger
-// const logger = new Console({ stdout: output, stderr: errorOutput });
-// use it like console
 
 const dataArray = [];
-// In stdout.log: count 5
 
 
 app.use((req, res, next) => {
-// write your logging code here
 //Grabs the User Agent
 var userGetData = req.get("User-agent");
 var newUserData = '"' + userGetData + '"';
 dataArray.push(newUserData);
-//logger.log(newUserData);
 
 //Grabs the Date/Time
 var DateTime = new Date();
 var formatTime = DateTime.toISOString();
 dataArray.push(formatTime);
-//logger.log(formatTime);
 
 //Grabs the Method (GET, POST, DELETE, etc.)
 var Method = req.method;
 dataArray.push(Method);
-//logger.log(Method);
 
 //Grabs the Resource
 var Resource = req.url;
 dataArray.push(Resource);
-//logger.log(Resource);
-//Grabs the HTTP Version
 
+//Grabs the HTTP Version
 var httpVer = req.httpVersion;
 dataArray.push("HTTP/" + httpVer);
-//logger.log(httpVer);
 
 //Grabs the Status Code
 var status = res.statusCode;
 dataArray.push(status);
-//logger.log(status);
-
-//logs the full array of initialData
-//logger.log(dataArray);
 
 let newArrayofStr = dataArray.join(',');
 let breakLineOnArr = newArrayofStr + "\n";
-
-console.log(newArrayofStr);
 
 fs.appendFile("/tmp/log.csv", breakLineOnArr, (err) => {
     if (err) throw err;
@@ -81,8 +61,6 @@ const csv=require('csvtojson')
 csv()
 .fromFile(csvFilePath)
 .then((jsonObj)=>{
-    console.log(jsonObj);
-    //logger.log(jsonObj);
 res.status(200).send(jsonObj);
 })
 });
